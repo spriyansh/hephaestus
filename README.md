22814657c9062884b8a3268d2b265308b5ac5480
spriyansh <34842808+spriyansh@users.noreply.github.com>
2021-06-06 15:29:35 +0100

    update

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# [Hephaestus](https://priyansh-srivastava.shinyapps.io/hephaestus-master/)
+# Hephaestus
 __Shiny Platform for Next-Gen data analysis__
 
 ---
 
-### Description
 
+[Click to see](https://priyansh-srivastava.shinyapps.io/hephaestus-master/)
 
 ### Motivation
 Hephaestus is the Greek god of blacksmiths, metalworking, carpenters, craftsmen, artisans, sculptors, and metallurgy. He served as the blacksmith of the gods and was worshipped in the manufacturing and industrial centres of Greece. Hephaestus crafted much of the magnificent equipment of the gods. Inspired by the name and _Hephaestus_ is a shiny based application specifically designed for the analysis of micro-array based data. It has been noticed that non-programmers face a broad array of challenges when it comes to data analysis and visualisations. To bridge the gap between the data scientists and biotechnologists, I present “Hephaestus” which is an end-to-end microarray data analysis tool developed with the power of shiny.
---
+++ interactiveFunctions/boxplotGGPLOT2Interactive.R
@@ -0,0 +1,32 @@
+################################################################################
+############################ Boxplot with plotly ###############################
+################################################################################
+
+
+################################################################################
+# Author: Priyansh Srivastava ##################################################
+# Contact: spriyansh29@gmail.com ###############################################
+# Dataset: https://zenodo.org/record/2529926/files/limma-voom_luminalpregnant-luminallactateDD;
+# https://zenodo.org/record/2529926/files/heatmap_genes ########################
+# https://zenodo.org/record/2529926/files/limma-voom_luminalpregnant-luminallactate#
+################################################################################
+
+
+# Library
+library(ggplot2)
+library(plotly)
+options(ggrepel.max.overlaps = Inf)
+
+# Reading Dataset 
+longExpSet <- read.csv("datasets/boxDataset.csv")
+
+# Making color palette
+pal <- c("#009E73", "#D55E00")
+
+# Designing plot
+interactiveBoix <- plot_ly(longExpSet, x = ~Sample, y = ~Expression, color = ~Group, type = "box",
+                           colors = pal)
+# Hiding the bar
+interactiveBoix <- interactiveBoix %>% config(displayModeBar = F)
+
+interactiveBoix
