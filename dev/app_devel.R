90ca8de571fa14125366442ef3b040848ec8c292
spriyansh <spriyansh29@gmail.com>
2024-05-09 22:07:19 +0200

    package_init

---
+++ dev/app_devel.R
@@ -0,0 +1,13 @@
+## Load Shiny
+library(shiny)
+
+# Source the base UI
+ui <- uiOutput("ui")
+
+# Source the base server
+server <- function(input, output, session) {
+    ui <- reactiveFileReader(1000, session, "dev/app.R", source)
+    output$ui <- renderUI(ui())
+}
+
+shinyApp(ui = ui, server = server)
